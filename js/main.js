7eeb0a97396201838caa3eb4ef0594cce340fdb6
Adriana Castillo <adricasra1993@gmail.com>
2025-09-25 14:05:23 -0500

    Cree el evento escucha DOMContentLoaded con el JSON.parse para convertir texto en formato JSON a un formato JS para guardar objetos en el LocarStorage

--- js/main.js
+++ js/main.js
@@ -1,56 +1,100 @@
 
 // Declaraaamos las variables globales
-const form = document.getElementById('formulario');
-const input = document.getElementById('nombre');
-const saludo = document.getElementById('saludo');
+const form = document.getElementById('formulario-curso');
+const inputCurso = document.getElementById('nombre');
+const inputProfesor = document.getElementById('profesor');
+const inputPrecio = document.getElementById('precio');
+const inputCiudad = document.getElementById('ciudad');
+const inputCupo = document.getElementById('cupo');
+const mensajeCurso = document.getElementById('mensaje-curso');
 const borrar = document.getElementById('btn-borrar');
+
 
 
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const nombreUsuario = input.value.trim();
+    const curso = inputCurso.value.trim();
+    const profesor = inputProfesor.value.trim();
+    const precio = inputPrecio.value.trim();
+    const ciudad = inputCiudad.value.trim();
+    const cupo = inputCupo.value.trim();
 
-    if (nombreUsuario == '') {
-        alert('Ingrese su nombre ');
+
+    if (curso == ''|| profesor ==''|| precio ==''|| ciudad ==''|| cupo =='') {
+        alert('Ingrese todos los datos ');
         return;
     }
+// En este ejercicio se construye un objeto con caracteristicas con sus diferentes claves y valores
+    const newCurso ={
+        nombre: curso,
+        profesor: profesor,
+        precio: precio,
+        ciudad: ciudad,
+        cupo: cupo
+    }
 
-    localStorage.setItem('usuario', nombreUsuario);
 
-    saludo.textContent = 'Bienvenido: ' + nombreUsuario;
 
-    input.value='';
+    localStorage.setItem('curso', JSON.stringify(newCurso));
+
+    form.reset();
 
 });
 
 
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    const usuarioGuardado = localStorage.getItem('usuario');
-
-    if (usuarioGuardado) {
-        saludo.textContent = 'Bienvenido de nuevo: ' + usuarioGuardado;
+document.addEventListener('DOMContentLoaded', ()=>{
+    const cursoCreado= localStorage.getItem('curso');
+// 
+    if (cursoCreado) {
+        const objetoCurso= JSON.parse(cursoCreado);
+        mensajeCurso.textContent = ' curso: ' + objetoCurso.nombre + ' profesor: ' + objetoCurso.profesor + ' precio: '+ objetoCurso.precio + ' ciudad: ' + objetoCurso.ciudad + ' cupo: ' + objetoCurso.cupo;
+        
+        
     }
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+    // mensajeCurso.textContent = 'Aqui se mostrara el curso creado: ' + newCurso;
+
+    // input.value='';
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//     const usuarioGuardado = localStorage.getItem('usuario');
+
+//     if (usuarioGuardado) {
+//         saludo.textContent = 'Bienvenido de nuevo: ' + usuarioGuardado;
+//     }
+
+// });
 
 
   
-borrar.addEventListener('click', () => {
-    localStorage.removeItem('usuario');
-    saludo.textContent = 'Bienvenido usuario indefinido';
+// borrar.addEventListener('click', () => {
+//     localStorage.removeItem('usuario');
+//     saludo.textContent = 'Bienvenido usuario indefinido';
 
-});
+// });
 
 
 
-document.addEventListener('DOMContentLoaded', () =>{
-    usuarioCreado = localStorage.getItem('usuario');
+// document.addEventListener('DOMContentLoaded', () =>{
+//     usuarioCreado = localStorage.getItem('usuario');
     
-});
+// });
 
 
 
